02b25c4c637fed8d04a620f86fd04505c473d7c7
mikashani <mikashani1993@gmail.com>
2017-03-30 16:02:20 +0300

    scrollList in mainWindow - mika

--- src/main/java/Client/GUI/MainWindow.java
+++ src/main/java/Client/GUI/MainWindow.java
@@ -1,5 +1,8 @@
 package Client.GUI;
 
+import Client.RequestOrganization.OrderInstruction;
+import Client.UserInterface;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.HeadlessException;
@@ -7,38 +10,38 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import javax.swing.*;
 
+
 public class MainWindow extends BasicFrame {
+    protected UserInterface UI;
     private JButton newOrderButton;
     private JButton recentOrdersButton;
     Insets inset;
     GridBagConstraints constraints;
+    private JList ordersList;
+    JScrollPane ordersScrollList;
 
     public MainWindow() throws HeadlessException, MalformedURLException {
-        Dimension buttonSize = new Dimension(screenSize.width/15, screenSize.height/20);
+        Dimension buttonSize = new Dimension(300, 100);
         this.constraints = new GridBagConstraints();
-        this.inset = new Insets(0, 0, 100, 400);
+        this.inset = new Insets(0, 0, 100, 50);
         this.constraints.insets = this.inset;
         this.constraints.gridwidth = 0;
         this.constraints.gridheight = 0;
         this.constraints.gridx = 0;
         this.constraints.gridy = 0;
-        this.constraints.ipadx = 0;
-        this.constraints.ipady = 0;
         this.newOrderButton = new JButton("New Order");
         this.newOrderButton.setName("newOrderButton");
-        this.newOrderButton.setPreferredSize( buttonSize );
+        this.newOrderButton.setPreferredSize(buttonSize);
         this.newOrderButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             }
         });
         this.mainPanel.add(this.newOrderButton, this.constraints);
-        this.inset = new Insets(0, 400, 100, 0);
+        this.inset = new Insets(0, 50, 100, 0);
         this.constraints.insets = this.inset;
-        this.constraints.gridwidth = 0;
-        this.constraints.gridheight = 0;
         this.constraints.gridx = 0;
         this.constraints.gridy = 0;
         this.constraints.ipadx = 0;
@@ -51,6 +54,21 @@ public class MainWindow extends BasicFrame {
             }
         });
         this.mainPanel.add(this.recentOrdersButton, this.constraints);
+        this.inset = new Insets(150, 0, 0, 0);
+        this.constraints.insets = this.inset;
+        //DefaultListModel listModel = new DefaultListModel<String>(UI.getOrders());
+       // OrderInstruction[] orderInstructionArray = (OrderInstruction[]) (UI.getOrders()).values().toArray();
+        //for tests
+        Object[] testArray = {"order1", "order2","order3"};
+        this.ordersList = new JList(testArray);
+        ordersList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        ordersList.setLayoutOrientation(JList.VERTICAL);
+        ordersList.setVisibleRowCount(-1);
+        ordersScrollList = new JScrollPane(ordersList);
+        ordersScrollList.setPreferredSize(new Dimension(250, 100));
+
+        this.mainPanel.add(this.ordersScrollList, this.constraints);
+
 
         this.init();
     }
