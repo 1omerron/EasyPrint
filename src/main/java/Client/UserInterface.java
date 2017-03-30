253c50d74939c97407d4a89902bab345551193ae
Your Name Here <hbarakosh@gmail.com>
2017-03-30 11:41:20 +0300

    Added print class

---
+++ src/main/java/Client/UserInterface.java
@@ -0,0 +1,88 @@
+package Client;
+import Client.RequestOrganization.FileInfo;
+import Client.RequestOrganization.FileInstruction;
+import Client.RequestOrganization.OrderInstruction;
+import Client.RequestOrganization.PageRangeInstruction;
+import Client.User.User;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Created by nimrod on 28/03/2017.
+ */
+public class UserInterface
+{
+    private String currentOrderId;
+    private User user;
+
+    public UserInterface(User user)
+    {
+        this.user = user;
+        currentOrderId = null;
+    }
+
+    /**
+     * @param firstPage
+     * @param lastPage
+     * @param printQuantity
+     */
+    public PageRangeInstruction createPageRangeInstruction(int firstPage, int lastPage, int printQuantity)
+    {
+        PageRangeInstruction range = new PageRangeInstruction(firstPage,lastPage,printQuantity);
+        return range;
+    }
+
+    /**
+     * @param orderId to set
+     */
+    public void updateCurrentOrderId(String orderId)
+    {
+        currentOrderId = orderId;
+    }
+
+    /**
+     * @param path to the file
+     * @return Fileinfo
+     */
+    public FileInfo createFileInfo(String path)
+    {
+        FileInfo fileInfo = new FileInfo(path, user.getUserId());
+        return fileInfo;
+    }
+    /**
+     * create new file instruction
+     */
+    public FileInstruction createFileInstruction(FileInfo fileInfo, PageRangeInstruction page)
+    {
+        FileInstruction fileIns = new FileInstruction(fileInfo, page);
+        return fileIns;
+    }
+
+    /**
+     * @param fileIns the file instruction to add to.
+     * @param pageRange the PageRange Instruction to add.
+     */
+    public void addPageRangeInstruction(FileInstruction fileIns, PageRangeInstruction pageRange)
+    {
+        fileIns.addPageRangeInstruction(pageRange);
+    }
+
+    /**
+     * add file instruction the given order or create new order if not exist
+     * @param fileIns the file instruction to add to order
+     */
+    public void addFileInstruction(FileInstruction fileIns)
+    {
+        if(!(user.getOrderInstructions().containsKey(currentOrderId)))
+        {//create this order
+            LinkedList<FileInstruction> list = new LinkedList<>();
+            list.addLast(fileIns);
+            OrderInstruction newOrder = new OrderInstruction(fileIns);
+        }
+        else
+        {//order exist
+            user.getOrderInstructions().get(currentOrderId).getInstructionsList().addLast(fileIns);
+        }
+    }
+}
